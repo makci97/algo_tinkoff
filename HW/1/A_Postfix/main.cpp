f60659c6c0c3a4b89202692447e217e8aeb7afe3
makci97 <makci.97@mail.ru>
2018-09-16 15:51:39 +0300

    Add postfix code

--- HW/1/A_Postfix/main.cpp
+++ HW/1/A_Postfix/main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
-#include <string>
+
+bool is_num(const std::string& s)
+{
+    return std::all_of(
+            s.begin(), s.end(),
+            [](unsigned char c){ return std::isdigit(c); }
+    );
+}
 
 int main() {
+    int num;
+    std::string str;
+    std::stack<int> numbers;
     std::ifstream in_stream;
     std::ofstream out_stream;
-    in_stream.open ("./postfix.txt");
-//    out_stream.open("postfix.out");
+    in_stream.open ("postfix.in");
+    out_stream.open("postfix.out");
 
-//    char c;
-    std::string str;
-    std::getline(in_stream, str);
-//    in_stream >> c;
-    std::cout << str;
-    std::cout << "12";
-//    out_stream << c;
+    while(!in_stream.eof()) {
+        in_stream >> str;
+        if(is_num(str)){
+            num = std::stoi(str);
+            numbers.push(num);
+        }
+        else {
+            switch(str[0]) {
+                case '+':
+                    num = numbers.top();
+                    numbers.pop();
+                    num += numbers.top();
+                    numbers.pop();
+                    numbers.push(num);
+                    break;
+                case '-':
+                    num = numbers.top();
+                    numbers.pop();
+                    num = numbers.top() - num;
+                    numbers.pop();
+                    numbers.push(num);
+                    break;
+                case '*':
+                    num = numbers.top();
+                    numbers.pop();
+                    num *= numbers.top();
+                    numbers.pop();
+                    numbers.push(num);
+                    break;
+            }
+        }
+    }
+    out_stream << numbers.top();
 
     in_stream.close();
-//    out_stream.close();
+    out_stream.close();
 
     return 0;
 }
