523cd44c60e9b9ced55c1215e17cc79864ab19e3
makci97 <makci.97@mail.ru>
2018-10-25 22:42:25 +0300

    Add spaces to B_Horse

--- HW/2/B_Horse/main.cpp
+++ HW/2/B_Horse/main.cpp
@@ -6,16 +6,16 @@
 
 int n_ways(std::vector<std::vector<std::pair<int, bool> > >& desk, int n, int m)
 {
-    if(n == 0 && m == 0)
+    if (n == 0 && m == 0)
         desk[0][0] = {1, true};
-    if(desk[n][m].second)
+    if (desk[n][m].second)
         return desk[n][m].first;
-    if(n >= 2 && m >= 1)
+    if (n >= 2 && m >= 1)
         desk[n][m].first += n_ways(desk, n - 2, m - 1);
-    if(n >= 1 && m >= 2)
+    if (n >= 1 && m >= 2)
         desk[n][m].first += n_ways(desk, n - 1, m - 2);
     desk[n][m].second = true;
-    
+
     return desk[n][m].first;
 }
 
@@ -24,13 +24,13 @@ int main() {
     int n, m;
     std::ifstream in_stream;
     std::ofstream out_stream;
-    in_stream.open ("input");
+    in_stream.open("input");
     out_stream.open("output");
 
     in_stream >> n >> m;
     std::vector<std::vector<std::pair<int, bool> > > desk;
     std::vector<std::pair<int, bool> > clear_row(m, {0, false});
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
         desk.push_back(clear_row);
 
     out_stream << n_ways(desk, n - 1, m - 1);
